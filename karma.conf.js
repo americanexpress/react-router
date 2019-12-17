161be2fc826b4717b8b0aeb748e0727d479d4173
James Singleton <james.singleton1@aexp.com>
2019-12-17 14:36:09 -0700

    chore(config): update karma test order

--- karma.conf.js
+++ karma.conf.js
@@ -1,72 +1,14 @@
 module.exports = (config) => {
-  const configuration = {
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-    // plugins starting with karma- are autoloaded
-    plugins: ['karma-chrome-launcher', 'karma-mocha'],
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
-
-    // list of files / patterns to load in the browser
-    files: [
-      'tests.webpack.js',
-    ],
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+  config.set({
+    frameworks: ['mocha', 'chai'],
+    files: ['test/**/*.js'],
     reporters: ['progress'],
-
-    // web server port
-    port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
+    port: 9876, // karma web server port
     colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'ChromeCanary', 'Firefox', 'Opera', 'IE'],
-
-    // e.g see https://swizec.com/blog/how-to-run-javascript-tests-in-chrome-on-travis/swizec/6647
-    customLaunchers: {
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox'],
-      },
-    },
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
+    browsers: ['ChromeHeadless'],
+    autoWatch: false,
+    // singleRun: false, // Karma captures browsers, runs the tests and exits
     concurrency: Infinity,
-  };
-
-  if (process.env.TRAVIS) {
-    configuration.browsers = ['Chrome_travis_ci'];
-  }
-
-  config.set(configuration);
+  });
 };
