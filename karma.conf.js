68f7998454615b17ff332bc0e4be89c9095e4c07
James Singleton <james.singleton1@aexp.com>
2019-12-17 14:44:37 -0700

    chore(config): update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,26 @@
+const webpack = require('webpack');
+
 module.exports = (config) => {
   config.set({
-    frameworks: [ 'mocha' ],
+    frameworks: ['mocha'],
     files: ['tests.webpack.js'],
+    preprocessors: {
+      'tests.webpack.js': ['webpack', 'sourcemap'],
+    },
+    webpack: {
+      devtool: 'cheap-module-inline-source-map',
+      module: {
+        loaders: [
+          { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+        ],
+      },
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('test'),
+          __DEV__: true,
+        }),
+      ],
+    },
     reporters: ['progress'],
     port: 9876, // karma web server port
     colors: true,
