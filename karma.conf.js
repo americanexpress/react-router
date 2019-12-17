ed83b561a10e08ad195227c1ccb92b4f215283f9
James Singleton <james.singleton1@aexp.com>
2019-12-17 14:50:57 -0700

    chore(config): update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,26 +1,46 @@
-const webpack = require('webpack');
+const webpack = require('webpack')
+module.exports = config => {
+  if (process.env.RELEASE)
+    config.singleRun = true
 
-module.exports = (config) => {
-  config.set({
-    frameworks: ['mocha'],
-    files: ['tests.webpack.js'],
+  const customLaunchers = {
+    // The ancient Travis Chrome that most projects use in CI.
+    ChromeCi: {
+      base: 'Chrome',
+      flags: [ '--no-sandbox' ]
+    }
+  }
+
+  const configuration = {
+    customLaunchers: customLaunchers,
+
+    browsers: [ 'Chrome' ],
+    frameworks: [ 'mocha' ],
+    reporters: [ 'mocha', 'coverage' ],
+
+    files: [
+      'tests.webpack.js'
+    ],
+
     preprocessors: {
-      'tests.webpack.js': ['webpack', 'sourcemap'],
+      'tests.webpack.js': [ 'webpack', 'sourcemap' ]
     },
+
     webpack: {
       devtool: 'cheap-module-inline-source-map',
       module: {
         loaders: [
-          { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
-        ],
+          { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
+        ]
       },
       plugins: [
         new webpack.DefinePlugin({
           'process.env.NODE_ENV': JSON.stringify('test'),
-          __DEV__: true,
-        }),
-      ],
+          __DEV__: true
+        })
+      ]
     },
+
     webpackServer: {
       noInfo: true
     },
@@ -28,14 +48,21 @@ module.exports = (config) => {
     coverageReporter: {
       type: 'lcov',
       dir: 'coverage'
-    },
-    reporters: ['progress'],
-    port: 9876, // karma web server port
-    colors: true,
-    logLevel: config.LOG_INFO,
-    browsers: ['ChromeHeadless'],
-    autoWatch: false,
-    // singleRun: false, // Karma captures browsers, runs the tests and exits
-    concurrency: Infinity,
-  });
-};
+    }
+  }
+
+  if (process.env.USE_CLOUD) {
+    configuration.browsers = Object.keys(customLaunchers)
+    configuration.reporters[0] = 'dots'
+    configuration.concurrency = 2
+
+    configuration.browserDisconnectTimeout = 10000
+    configuration.browserDisconnectTolerance = 3
+
+    if (process.env.TRAVIS) {
+      configuration.browsers = Object.keys(customLaunchers)
+    }
+  }
+
+  config.set(configuration)
+}
