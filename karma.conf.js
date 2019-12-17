aa495f443e1dcc9273499194a2bb57eaab4cf7d0
Jonathan Adshead <jonathan.adshead@aexp.com>
2019-12-17 14:59:16 -0700

    chore(): revert karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,62 @@
 const webpack = require('webpack')
+const projectName = require('./package').name
+
 module.exports = config => {
   if (process.env.RELEASE)
     config.singleRun = true
 
   const customLaunchers = {
+    // Browsers to run on BrowserStack.
+    BS_Chrome: {
+      base: 'BrowserStack',
+      os: 'Windows',
+      os_version: '10',
+      browser: 'chrome',
+      browser_version: '47.0'
+    },
+    BS_Firefox: {
+      base: 'BrowserStack',
+      os: 'Windows',
+      os_version: '10',
+      browser: 'firefox',
+      browser_version: '43.0'
+    },
+    BS_Safari: {
+      base: 'BrowserStack',
+      os: 'OS X',
+      os_version: 'El Capitan',
+      browser: 'safari',
+      browser_version: '9.0'
+    },
+    BS_MobileSafari8: {
+      base: 'BrowserStack',
+      os: 'ios',
+      os_version: '8.3',
+      browser: 'iphone',
+      real_mobile: false
+    },
+    BS_MobileSafari9: {
+      base: 'BrowserStack',
+      os: 'ios',
+      os_version: '9.1',
+      browser: 'iphone',
+      real_mobile: false
+    },
+    BS_InternetExplorer10: {
+      base: 'BrowserStack',
+      os: 'Windows',
+      os_version: '8',
+      browser: 'ie',
+      browser_version: '10.0'
+    },
+    BS_InternetExplorer11: {
+      base: 'BrowserStack',
+      os: 'Windows',
+      os_version: '10',
+      browser: 'ie',
+      browser_version: '11.0'
+    },
+
     // The ancient Travis Chrome that most projects use in CI.
     ChromeCi: {
       base: 'Chrome',
@@ -11,7 +64,7 @@ module.exports = config => {
     }
   }
 
-  const configuration = {
+  config.set({
     customLaunchers: customLaunchers,
 
     browsers: [ 'Chrome' ],
@@ -49,20 +102,28 @@ module.exports = config => {
       type: 'lcov',
       dir: 'coverage'
     }
-  }
+  })
 
   if (process.env.USE_CLOUD) {
-    configuration.browsers = Object.keys(customLaunchers)
-    configuration.reporters[0] = 'dots'
-    configuration.concurrency = 2
+    config.browsers = Object.keys(customLaunchers)
+    config.reporters[0] = 'dots'
+    config.concurrency = 2
 
-    configuration.browserDisconnectTimeout = 10000
-    configuration.browserDisconnectTolerance = 3
+    config.browserDisconnectTimeout = 10000
+    config.browserDisconnectTolerance = 3
 
     if (process.env.TRAVIS) {
-      configuration.browsers = Object.keys(customLaunchers)
+      config.browserStack = {
+        project: projectName,
+        build: process.env.TRAVIS_BUILD_NUMBER,
+        name: process.env.TRAVIS_JOB_NUMBER
+      }
+
+      config.singleRun = true
+    } else {
+      config.browserStack = {
+        project: projectName
+      }
     }
   }
-
-  config.set(configuration)
 }
